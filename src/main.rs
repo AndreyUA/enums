aeb2a3033dbf451b11ca7a2a32371454dc2ecdad
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-04 21:55:56 +0200

    methods in enums

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 enum IpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
+}
+
+impl IpAddr {
+    fn print(&self) {
+        println!("{:?}", self)
+    }
 }
 
 fn route(ip_kind: IpAddr) {}
@@ -10,4 +17,7 @@ fn main() {
     let six = IpAddr::V6(String::from("::1"));
 
     println!("Hello, world!");
+
+    let test = IpAddr::V6(String::from("This is V6"));
+    test.print();
 }
